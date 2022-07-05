a435c172374ce186f094e2ebbcb235b72a6402a5
AzizAmiraslanov <aziz.amiraslan@gmail.com>
2022-07-05 23:04:49 +0300

    Javascript dərslərinə giriş

---
+++ script.js
@@ -0,0 +1,15 @@
+let p = document.getElementById("paragraf1");
+
+function sol (){
+    p.style.textAlign ="left";
+}
+
+
+function merkez (){
+    p.style.textAlign ="center";
+}
+
+
+function sag (){
+    p.style.textAlign ="right";
+}
